64b0f327f3e4bb8229e8334e1b1910dcb8b7b68c
MatiRosero <gekidasa@gmail.com>
2017-04-06 14:46:49 -0600

    removed unused div

--- single-mro-team.php
+++ single-mro-team.php
@@ -15,24 +15,20 @@
 
 get_header(); ?>
 
-<div class="row">
+<div class="row column">
 
-	<div class="medium-10 large-8 medium-centered columns">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/team/content', 'single' ); ?>
 
-					<?php get_template_part( 'template-parts/team/content', 'single' ); ?>
+			<?php endwhile; // End of the loop. ?>
 
-				<?php endwhile; // End of the loop. ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
-			</main><!-- #main -->
-		</div><!-- #primary -->
-
-	</div><!-- .columns -->
-
-</div><!-- .row -->
+</div><!-- .row.column -->
 
 <?php get_footer();
