5ed1ab24daf8cb9b057ce0feaa7dcf253a5cdbae
MatiRosero <gekidasa@gmail.com>
2017-04-08 12:29:56 -0600

    added rest of thml structure

--- template-parts/page/content-schedule.php
+++ template-parts/page/content-schedule.php
@@ -130,6 +130,25 @@ endif;
 				?>
 			</ul>
 		</div><!-- .events -->
+		<div class="event-modal">
+			<header class="header">
+				<div class="content">
+					<span class="event-date"></span>
+					<h3 class="event-name"></h3>
+				</div>
+
+				<div class="header-bg"></div>
+			</header>
+
+			<div class="body">
+				<div class="event-info"></div>
+				<div class="body-bg"></div>
+			</div>
+
+			<a href="#0" class="close">Close</a>
+		</div>
+
+		<div class="cover-layer"></div>
 	</section><!-- .mro-schedule -->
 
 	<footer class="entry-footer">
