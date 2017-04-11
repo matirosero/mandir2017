223e0495eeec4e8674cfd5c6a7fef2d83f4fab7a
MatiRosero <gekidasa@gmail.com>
2017-04-10 22:46:28 -0600

    currency sign

--- template-parts/page/content-pricing.php
+++ template-parts/page/content-pricing.php
@@ -15,7 +15,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 			<h3><?php _e('Single class', 'mandir'); ?></h3>
 			<p><?php echo $single_class['single_class_description']; ?></p>
 			<div class="callout secondary">
-				<span class="price"><?php echo $single_class['single_class_price']; ?></span>
+				<span class="price">₡<?php echo $single_class['single_class_price']; ?></span>
 				<span class="price-description"><?php _e('1 class', 'mandir'); ?></span>
 			</div>
 		</li>
@@ -23,7 +23,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 			<h3><?php _e('Membership', 'mandir'); ?></h3>
 			<p><?php echo $signup_fee['signup_description']; ?></p>
 			<div class="callout secondary">
-				<span class="price"><?php echo $signup_fee['signup_price']; ?></span>
+				<span class="price">₡<?php echo $signup_fee['signup_price']; ?></span>
 				<span class="price-description"><?php _e('Per year', 'mandir'); ?></span>
 			</div>
 
@@ -38,7 +38,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 						<ul>
 							<li>
 								<div class="callout secondary">
-									<span class="price"><?php echo $monthly_price['monthly_1weekly']; ?></span>
+									<span class="price">₡<?php echo $monthly_price['monthly_1weekly']; ?></span>
 									<span class="price-description">
 										<?php _e('1 class per week'); ?><br />
 										<?php _e('4 classes per week'); ?>
@@ -48,7 +48,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 
 							<li>
 								<div class="callout secondary">
-									<span class="price"><?php echo $monthly_price['monthly_2weekly']; ?></span>
+									<span class="price">₡<?php echo $monthly_price['monthly_2weekly']; ?></span>
 									<span class="price-description">
 										<?php _e('2 classes per week'); ?><br />
 										<?php _e('8 classes per week'); ?>
@@ -58,7 +58,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 
 							<li>
 								<div class="callout secondary">
-									<span class="price"><?php echo $monthly_price['monthly_3weekly']; ?></span>
+									<span class="price">₡<?php echo $monthly_price['monthly_3weekly']; ?></span>
 									<span class="price-description">
 										<?php _e('3 classes per week'); ?><br />
 										<?php _e('12 classes per week'); ?>
@@ -68,7 +68,7 @@ $monthly_prices = $pricing_settings['monthly_prices'];
 
 							<li>
 								<div class="callout primary">
-									<span class="price"><?php echo $monthly_price['monthly_unlimited']; ?></span>
+									<span class="price">₡<?php echo $monthly_price['monthly_unlimited']; ?></span>
 									<span class="price-description">
 										<?php _e('Unlimited classes'); ?>
 									</span>
