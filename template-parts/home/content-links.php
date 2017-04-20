aaa504c15eaecff5b044a468a726078753b58838
MatiRosero <gekidasa@gmail.com>
2017-04-19 22:47:36 -0600

    spotlight style

--- template-parts/home/content-links.php
+++ template-parts/home/content-links.php
@@ -7,34 +7,26 @@
 <section class="block-links expanded row small-up-1 medium-up-2 large-up-4 small-collapse" data-equalizer data-equalize-on="small">
 	<div class="column column-block">
 		<a href="<?php echo get_permalink( get_page_by_path( 'horario' ) ); ?>" id="block-link-classes" class="block-link" data-equalizer-watch>
-			<div>
-				<h3>Clases de Yoga</h3>
-				<p>Horarios, modalidades y precios</p>
-			</div>
+			<h3>Clases de Yoga</h3>
+			<p>Horarios, modalidades y precios</p>
 		</a>
 	</div>
 	<div class="column column-block">
 		<a href="<?php echo get_permalink( get_page_by_path( 'entrenamiento' ) ); ?>" id="block-link-training" class="block-link" data-equalizer-watch>
-			<div>
-				<h3>Entrenamiento para Profesores</h3>
-				<p>Información, fechas y matrícula</p>
-			</div>
+			<h3>Entrenamiento para Profesores</h3>
+			<p>Información, fechas y matrícula</p>
 		</a>
 	</div>
 	<div class="column column-block">
 		<a href="<?php echo get_permalink( get_page_by_path( 'masaje-tailandes' ) ); ?>" id="block-link-thai" class="block-link" data-equalizer-watch>
-			<div>
-				<h3>Masaje tailandés</h3>
-				<p>4 niveles de certificación</p>
-			</div>
+			<h3>Masaje tailandés</h3>
+			<p>4 niveles de certificación</p>
 		</a>
 	</div>
 	<div class="column column-block">
 		<a href="<?php echo get_permalink( get_page_by_path( 'tienda' ) ); ?>" id="block-link-store" class="block-link" data-equalizer-watch>
-			<div>
-				<h3>Tienda</h3>
-				<p>Mats de yoga y otros productos</p>
-			</div>
+			<h3>Tienda</h3>
+			<p>Mats de yoga y otros productos</p>
 		</a>
 	</div>
 </section><!-- .page-section -->
--- template-parts/home/content-spotlight.php
+++ template-parts/home/content-spotlight.php
@@ -2,14 +2,14 @@
 $directory_url = 'http://www.yoga-mandir.com/certificados/';
 ?>
 
-<aside class="spotlight expanded collapse row">
+<aside class="spotlight expanded collapse row" data-equalizer data-equalize-on="medium">
 	<div class="spotlight-image medium-6 columns hide-for-small-only">
 		<a href="<?php echo $directory_url; ?>" id="" class="" data-equalizer-watch>
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-home.jpg" alt="Profesor en clase de yoga" />
 		</a>
 	</div>
 	<div class="medium-6 columns">
-		<div class="spotlight-text">
+		<div class="spotlight-text" data-equalizer-watch>
 			<h2>¡Profesores certificados en tu zona!</h2>
 			<p>Ingresá a la Red Mandir Certificados y podrás buscar profesores de yoga y masajistas certificados por Yoga Mandir en tu comunidad.</p>
 			<a href="<?php echo $directory_url; ?>">Ir a Red Mandir Certificados</a>
