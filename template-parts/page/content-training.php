b71bedda43cc5a55c68c1fbf729d193a18e2cbcf
MatiRosero <gekidasa@gmail.com>
2017-06-05 23:13:23 -0600

    sidebar first small

--- template-parts/page/content-training.php
+++ template-parts/page/content-training.php
@@ -18,70 +18,7 @@ $state = get_post_meta($post->ID, 'mro_training_state', true);
 
 	<div class="row entry-content">
 
-		<div class="large-8 columns">
-
-			<?php
-			if ( get_post_meta($post->ID, 'mro_training_curriculum', true) ) :
-				$curriculum = get_post_meta($post->ID, 'mro_training_curriculum', true);
-				?>
-				<h2>Curriculum</h2>
-				<ul>
-				<?php
-				foreach ( $curriculum as $module ) {
-					echo '<li><strong>'.$module['title'].':</strong> '.$module['length'].'<br />
-						'.$module['description'].'</li>';
-				} ?>
-				</ul>
-			<?php endif; ?>
-
-			<?php
-			if ( get_post_meta($post->ID, 'mro_training_recs', true) ) :
-				$recs = get_post_meta($post->ID, 'mro_training_recs', true);
-				?>
-				<h2>Recomendaciones</h2>
-				<ul>
-				<?php
-				foreach ( $recs as $rec ) {
-					echo '<li>'.$rec['rec'].'</li>';
-				} ?>
-				</ul>
-			<?php endif; ?>
-
-			<?php
-			if ( get_post_meta($post->ID, 'mro_training_certification', true) ) :
-				$certification_list = get_post_meta($post->ID, 'mro_training_certification', true);
-				?>
-				<h2>Reconocimiento</h2>
-				<ul>
-				<?php
-				foreach ( $certification_list as $li ) {
-					echo '<li>'.$li['point'].'</li>';
-				} ?>
-				</ul>
-			<?php endif; ?>
-
-			<h2>Profesorado</h2>
-			<?php
-			echo get_post_meta($post->ID, 'mro_training_teachers', true);
-			?>
-
-
-
-			<?php
-			if ( get_post_meta($post->ID, 'mro_training_howtoenroll', true) ) :
-				$steps = get_post_meta($post->ID, 'mro_training_howtoenroll', true);
-				?>
-				<h2>Proceso de matrícula</h2>
-				<ol>
-				<?php
-				foreach ( $steps as $step ) {
-					echo '<li>'.$step['point'].'</li>';
-				} ?>
-				</ol>
-			<?php endif; ?>
-		</div>
-
-		<div class="large-4 columns">
+		<div class="large-4 large-push-8 columns">
 			<div class="sidebar">
 				<h2>Próximo entrenamiento</h2>
 
@@ -151,6 +88,70 @@ $state = get_post_meta($post->ID, 'mro_training_state', true);
 
 			</div>
 		</div>
+		<div class="large-8 large-pull-4 columns">
+
+			<?php
+			if ( get_post_meta($post->ID, 'mro_training_curriculum', true) ) :
+				$curriculum = get_post_meta($post->ID, 'mro_training_curriculum', true);
+				?>
+				<h2>Curriculum</h2>
+				<ul>
+				<?php
+				foreach ( $curriculum as $module ) {
+					echo '<li><strong>'.$module['title'].':</strong> '.$module['length'].'<br />
+						'.$module['description'].'</li>';
+				} ?>
+				</ul>
+			<?php endif; ?>
+
+			<?php
+			if ( get_post_meta($post->ID, 'mro_training_recs', true) ) :
+				$recs = get_post_meta($post->ID, 'mro_training_recs', true);
+				?>
+				<h2>Recomendaciones</h2>
+				<ul>
+				<?php
+				foreach ( $recs as $rec ) {
+					echo '<li>'.$rec['rec'].'</li>';
+				} ?>
+				</ul>
+			<?php endif; ?>
+
+			<?php
+			if ( get_post_meta($post->ID, 'mro_training_certification', true) ) :
+				$certification_list = get_post_meta($post->ID, 'mro_training_certification', true);
+				?>
+				<h2>Reconocimiento</h2>
+				<ul>
+				<?php
+				foreach ( $certification_list as $li ) {
+					echo '<li>'.$li['point'].'</li>';
+				} ?>
+				</ul>
+			<?php endif; ?>
+
+			<h2>Profesorado</h2>
+			<?php
+			echo get_post_meta($post->ID, 'mro_training_teachers', true);
+			?>
+
+
+
+			<?php
+			if ( get_post_meta($post->ID, 'mro_training_howtoenroll', true) ) :
+				$steps = get_post_meta($post->ID, 'mro_training_howtoenroll', true);
+				?>
+				<h2>Proceso de matrícula</h2>
+				<ol>
+				<?php
+				foreach ( $steps as $step ) {
+					echo '<li>'.$step['point'].'</li>';
+				} ?>
+				</ol>
+			<?php endif; ?>
+		</div>
+
+
 
 	</div><!-- .entry-content -->
 
