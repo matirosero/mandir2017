e893b7e5bf482f6c46d33a7e5463289173ae92df
MatiRosero <gekidasa@gmail.com>
2017-06-08 16:51:45 -0600

    store template files created

---
+++ page-templates/template-store.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Store
+ * The template for displaying Store page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Mandir
+ */
+
+get_header(); ?>
+
+<div class="row column">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/page/content', 'store' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</div><!-- .row -->
+
+<?php get_footer(); ?>
---
+++ template-parts/page/content-store.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template part for displaying teacher training content.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Mandir
+ */
+?>
+
+<?php
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php get_template_part( 'template-parts/page/content', 'header-intro' ); ?>
+
+	<div class="row entry-content">
+
+hi
+
+
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
