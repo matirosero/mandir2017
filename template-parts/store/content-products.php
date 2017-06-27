93820acbb0a116b78ac457e1bd2c416db46e8b60
MatiRosero <gekidasa@gmail.com>
2017-06-26 22:02:55 -0600

    store spacing

--- template-parts/store/content-products.php
+++ template-parts/store/content-products.php
@@ -4,17 +4,19 @@ $products = get_post_meta($id, 'mro_store_products', true);
 ?>
 
 
-<div class="row entry-content medium-up-2" data-equalizer data-equalize-on="medium">
+<div class="row store-products page-section medium-up-2" data-equalizer data-equalize-on="medium">
+
+	<h2 class="section-title"><?php _e('Other products', 'mandir'); ?></h2>
 	<?php
 	foreach ($products as $product) { ?>
-		<article class="column column-block" data-equalizer-watch>
+		<article class="column column-block product" >
 
 			<?php
 				$img_id = $product['image'][0];
 
 				$imgsrcset = wp_get_attachment_image($img_id, 'full');
 
-				echo '<div class="">'.$imgsrcset.'</div>';
+				echo '<div class="product-image">'.$imgsrcset.'</div>';
 			?>
 
 			<h3><?php echo $product['title']; ?></h3>
