7973ab20edf7e0b3dc2e9f0de18a0bac70d3c70b
MatiRosero <gekidasa@gmail.com>
2017-06-30 23:06:29 -0600

    title over hero

--- template-parts/page/content-header-image.php
+++ template-parts/page/content-header-image.php
@@ -1,8 +1,12 @@
 <?php
 
 // If a regular post or page, and not the front page, show the featured image.
+?>
 
+<div class="hero-header">
+	<?php the_post_thumbnail(); ?>
+	<div class="hero-content">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</div>
 
-echo '<div class="hero-header">';
-the_post_thumbnail();
-echo '</div><!-- .hero-header -->';
+</div><!-- .hero-header -->
