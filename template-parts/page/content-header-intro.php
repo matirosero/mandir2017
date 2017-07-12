f74b4b079577555805f968e52eba0863a2c2febd
MatiRosero <gekidasa@gmail.com>
2017-07-11 23:28:57 -0600

    page intro to about us

--- template-parts/page/content-header-intro.php
+++ template-parts/page/content-header-intro.php
@@ -9,10 +9,19 @@
 ?>
 
 	<header class="entry-header">
-		<?php 
+		<?php
 		if ( !has_post_thumbnail() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' ); 
+			the_title( '<h1 class="entry-title">', '</h1>' );
 		endif;
-		?>
-		<?php the_content(); ?>
+
+
+		if ( is_page_template( array( 
+				'page-templates/template-events.php',
+				'page-templates/template-store.php',
+				'page-templates/template-training.php',
+			) ) ) :
+			the_content(); 
+		else:
+			echo wpautop( get_post_meta($post->ID, 'mro_page_intro', true) );
+		endif;?>
 	</header><!-- .entry-header -->
