6b71737c395aeadfe9c24de74ced13b2e6638b10
MatiRosero <gekidasa@gmail.com>
2017-07-17 12:21:00 -0600

    images to profile

--- single-mro-team.php
+++ single-mro-team.php
@@ -15,20 +15,28 @@
 
 get_header(); ?>
 
-<div class="row column">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/team/content', 'single' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
 
-			<?php endwhile; // End of the loop. ?>
+				<div class="row">
+					<div class="medium-10 medium-centered large-9 columns">
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					<?php get_template_part( 'template-parts/team/content', 'single' ); ?>
 
-</div><!-- .row.column -->
+					</div><!-- .columns -->
+				</div><!-- .row -->
+
+			</article><!-- #post-## -->
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
 
 <?php get_footer();
--- template-parts/team/content-single.php
+++ template-parts/team/content-single.php
@@ -10,12 +10,26 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php
+		if ( has_post_thumbnail() ) : ?>
+			<div class="profile-image">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		<?php endif;
+		the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
+
+	<?php
+	if ( get_post_meta( $id, 'mro_team_secondary_image', true ) ) :
+		$img_id = get_post_meta($id, 'mro_team_secondary_image', true);
+		$imgsrcset = wp_get_attachment_image($img_id, 'full');
+		echo '<div class="profile-image-secondary">'.$imgsrcset.'</div>';
+	endif;
+	?>
 
 	<footer class="entry-footer">
 		<?php
