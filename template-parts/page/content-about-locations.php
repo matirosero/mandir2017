a3b721559b8a15d6064c29bc6224d78e66d98e74
MatiRosero <gekidasa@gmail.com>
2017-07-17 20:59:51 -0600

    about us pulls from db everything

--- template-parts/page/content-about-locations.php
+++ template-parts/page/content-about-locations.php
@@ -11,11 +11,26 @@
 <div class="row">
 
 	<div class="medium-6 columns">
-		MAP
+	<?php
+		$map_shortcode = get_post_meta( $id, 'mro_about_map_shortcode', true );
+		echo do_shortcode( $map_shortcode );
+		?>
 	</div><!-- .columns -->
 
 	<div class="medium-6 columns">
-		Locations
+		<h2>Dónde estamos</h2>
+
+		<?php
+		$locations = get_post_meta( $id, 'mro_about_locations', true ); ?>
+
+		<ul class="locations">
+			<?php foreach ( $locations as $location ) {
+				echo '<li><h4>'.$location['name'].'</h4>
+					<p>'.$location['address'].'</p>
+					<p>Tel. '.$location['tel'].'</p></li>';
+			}
+			?>
+		</ul>
 	</div><!-- .spotlight-image -->
 
 </div><!-- .row -->
--- template-parts/page/content-about.php
+++ template-parts/page/content-about.php
@@ -9,6 +9,8 @@
 ?>
 
 <?php
+
+
 ?>
 
 <div class="spotlight expanded collapse row" data-equalizer data-equalize-on="medium">
@@ -20,12 +22,17 @@
 	</div><!-- .columns -->
 
 	<div class="spotlight-image medium-6 large-7 columns hide-for-small-only" data-equalizer-watch>
-		<?php the_content( ); ?>
+		<?php
+		if ( get_post_meta( $id, 'mro_about_secondary_image', true ) ) :
+			$img_id = get_post_meta($id, 'mro_about_secondary_image', true);
+			$imgsrcset = wp_get_attachment_image($img_id, 'full');
+			echo $imgsrcset;
+		endif;
+		?>
 	</div><!-- .spotlight-image -->
 
 </div><!-- .row -->
 
-<?php //get_template_part( 'template-parts/store/content', 'spotlight' ); ?>
 
 
 
