2087e1ba187cc7356f73d63cc2d1d137c16824a3
MatiRosero <gekidasa@gmail.com>
2017-07-26 16:18:28 -0600

    virabhadra template

---
+++ page-templates/template-vira.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Certificacion Virabhadra
+ * The template for displaying Teacher Training program information.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Mandir
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<div class="row column">
+					<?php get_template_part( 'template-parts/page/content', 'header-intro' ); ?>
+				</div><!-- .row column -->
+
+				<div class="row">
+					<?php get_template_part( 'template-parts/page/content', 'training' ); ?>
+				</div><!-- .row -->
+
+			</article><!-- #post-## -->
+
+		<?php endwhile; // End of the loop. ?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
