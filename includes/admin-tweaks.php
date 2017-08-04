0cdb58ca2ff5e3c0b7a1e6b0ad543acc698909ba
MatiRosero <gekidasa@gmail.com>
2017-08-04 15:11:41 -0600

    caldera style and tags for pages

--- includes/admin-tweaks.php
+++ includes/admin-tweaks.php
@@ -16,3 +16,12 @@ function remove_menus(){
   
 }
 add_action( 'admin_menu', 'remove_menus' );
+
+function mandir_tax_to_pages() {  
+  // Add tag metabox to page
+  register_taxonomy_for_object_type('post_tag', 'page'); 
+  // Add category metabox to page
+  // register_taxonomy_for_object_type('category', 'page');  
+}
+ // Add to the admin_init hook of your theme functions.php file 
+add_action( 'init', 'mandir_tax_to_pages' );
