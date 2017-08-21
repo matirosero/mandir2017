1e79a5b782e7d72372003b07214569d107c9ebc6
MatiRosero <gekidasa@gmail.com>
2017-08-20 18:42:38 -0600

    spotlight home srcset

--- template-parts/home/content-spotlight.php
+++ template-parts/home/content-spotlight.php
@@ -5,7 +5,12 @@ $directory_url = 'http://www.yoga-mandir.com/certificados/';
 <aside class="spotlight expanded collapse row" data-equalizer data-equalize-on="medium">
 	<div class="spotlight-image medium-6 columns hide-for-small-only">
 		<a href="<?php echo $directory_url; ?>" id="" class="" data-equalizer-watch>
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-home.jpg" alt="Profesor en clase de yoga" />
+			<img srcset="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-directory-1280x850.jpg 1280w,
+			<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-directory-1024x680.jpg 1024w,
+			<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-directory-20.jpg 20w" 
+			sizes="(min-width: 640px) 50vw, 1vw" 
+			src="<?php echo get_stylesheet_directory_uri(); ?>/assets/dist/img/spotlight-directory-1280x850.jpg" 
+			alt="Profesor en clase de yoga" />
 		</a>
 	</div>
 	<div class="medium-6 columns">
